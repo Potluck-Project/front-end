cfb0f4a1e80c56f0320efbb7b974f1bfd9b19dde
mariusmoldovan <biris_21@yahoo.com>
2021-06-22 13:07:18 -0500

    convert Login to Login.css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,63 +1,8 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import '../assets/css/Login.css'
 import axios from 'axios';
-import img from '../assets/Images/1.jpeg';
 
-const LogBase = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
- 
-    background-image:url(${img});
-    border: 2px solid red;
-    height: 100%; 
-    width: 100%;
-    background-size: cover;
-    
-`;
-const LogForm = styled.form`
-    overflow: hidden;
-    background-color: #F0F2F2;
-    justify-content: center;
-    align-items: stretch;
-    text-align: center;
-    width: 25%;
-    padding: 1rem;
-    margin: 1rem;
-    border-radius: 5px;
-`;
-const LogLgd = styled.legend`
-    font-size: 1rem;
-`;
-const LogLabel = styled.label`
-    display: inline;
-    font-size: 1rem;
-    padding: 0 .5rem;
-`;
-const LogInput = styled.input`
-    display: inline;
-    border: 1px solid #999999;
-    border-radius: 3px;
-    margin-bottom: 1rem;
-    font-size: 1rem;
-`;
-const LogBorder = styled.fieldset`
-    border-radius: 5px;
-    padding: 1rem;
-`;
-const LogBtn = styled.button`
-    margin: auto 15%;
-    width: 10rem;
-    height: 3rem;
-    background-color: #30BF45;
-    border: 1px solid #999999;
-    border-radius: 10px;
-    color: white;
-    :hover{
-        background-color: #C4F2D0;
-        color: black;
-    }
-`;
+
 
 const Login = props => {
     const [login, setLogin] = useState({username:"", password:""});
@@ -79,20 +24,20 @@ const Login = props => {
 
     return (
 
-        <LogBase>
-            <LogForm onSubmit = {logUser}>
-                <LogBorder>
-                    <LogLgd><h1>Login</h1></LogLgd>
-                    <LogLabel htmlFor='username' />User Name
-                    <LogInput
+        <div className="logBase">
+            <form className ="logForm"onSubmit = {logUser}>
+                <fieldset>
+                    <h2 className ="logLgd">Login</h2>
+                    <label className ="logLabel" htmlFor='username' />User Name
+                    <input
                         placeholder="Username"
                         id='username'
                         name="username"
                         value={login.username}
                         onChange={updateLogin}
                     />
-                    <LogLabel htmlFor='password' />Password
-                    <LogInput
+                    <label htmlFor='password' />Password
+                    <input
                         type="password"
                         id='password'
                         placeholder="Password"
@@ -100,10 +45,11 @@ const Login = props => {
                         value={login.password}
                         onChange = {updateLogin}
                     />
-                    <LogBtn>Login</LogBtn>
-                </LogBorder>
-            </LogForm>
-        </LogBase>
+                    <button>Login</button>
+                    
+                </fieldset>
+            </form>
+        </div>
 
     )
 }
