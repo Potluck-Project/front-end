7a1e0b2a5e76113b0408ebe5c7ec5a3567c0631d
kazden.jolley <kazden.jolley@gmail.com>
2021-06-24 14:56:30 -0600

    eventpage delete and edit buttons working

--- src/components/EventPage.js
+++ src/components/EventPage.js
@@ -1,6 +1,6 @@
 import '../assets/css/Event.css';
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, NavLink } from 'react-router-dom';
 
 import axiosWithAuth from '../utils/axiosWithAuth';
 
@@ -18,43 +18,58 @@ const EventPage = (props) => {
         setEvent(res.data);
         setAttendees(res.data.attendees);
         setItems(res.data.items);
-        console.log('res: ', res);
       })
       .catch(err => {
         console.log('err: ', err);
       })
 
   }, [])
+
+  const toEdit = () => {
+    window.location.pathname = `edit-event/${id}`;
+  }
+
+  const handleDelete = () => {
+    axiosWithAuth()
+      .delete(`api/event/${id}`)
+      .then(res => {
+        console.log('res: ', res);
+      })
+      .catch(err => {
+        console.log('err: ', err);
+      })
+  }
   return (
     <div className='container'>
       <div className='event_wrapper'>
         <h1>{event.event_name}</h1>
         <div className='event_details'>
           <div className='div_details'>
-            <h2>Date:</h2><h3>{event.event_date}<br/>{event.event_time}</h3>
-            <h2>Location:</h2><h3>{event.event_street_address}<br/>{event.event_city}, {event.event_state}<br/>{event.event_zip}</h3>
+            <h3>Date:</h3><h4>{event.event_date}<br/>{event.event_time}</h4>
+            <h4>Location:</h4><h4>{event.event_street_address}<br/>{event.event_city}, {event.event_state}<br/>{event.event_zip}</h4>
           </div>
-          <div className='div_list'>
+          <div className='div_attendees'>
           <h2>Attendees:</h2>
           {
-            attendees.map(person => {
-              return <h3>{person}</h3>
+            attendees.map((person, idx) => {
+              return <h3 key={idx}>{person}</h3>
             })
           }
           </div>
-          <div className='div_list'>
+          <div className='div_items'>
           <h2>Items:</h2>
+          <ul>
           {
             items.map(item => {
-
-              return <h3>{item.name}</h3>
+              return <li key={item.event_item_id}>{item.quantity} - {item.item_name}</li>
             })
           }
+          </ul>
           </div>
           <div className='div_buttons'>
-          <button>Edit Event</button>
+          <button onClick={toEdit} >Edit Event</button>
           &nbsp;
-          <button>Delete Event</button>
+          <button className='delete-button' onClick={handleDelete}>Delete Event</button>
           </div>
         </div>
       </div>
